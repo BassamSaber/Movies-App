1eac2eb77b3b5aa040ce99a1b105777042f54fb1
Bassam Saber <bsaber@panarab-media.com>
2024-09-12 23:34:45 +0300

    - add UI Testing for HomeScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/LICENSE.md"
+            excludes += "META-INF/LICENSE-notice.md"
         }
     }
 }
@@ -110,6 +112,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("io.mockk:mockk-android:1.13.12")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
---
+++ app/src/androidTest/java/com/samz/banquemisr/challenge05/helper/TestEntity.kt
@@ -0,0 +1,33 @@
+package com.samz.banquemisr.challenge05.helper
+
+import com.samz.banquemisr.challenge05.data.mappers.toMovie
+import com.samz.banquemisr.challenge05.data.remote.model.MovieDto
+import com.samz.banquemisr.challenge05.data.remote.model.MoviesListDto
+import com.samz.banquemisr.challenge05.presentation.components.carsoule.toCarouselDataItem
+import com.samz.banquemisr.challenge05.presentation.home.HomeData
+
+val expectMovieDto = MovieDto(
+    id = 1,
+    title = "Trap",
+    originalTitle = "Trap",
+    originalLanguage = "en",
+    posterPath = "/jwoaKYVqPgYemFpaANL941EF94R.jpg",
+    backdropPath = "/p5kpFS0P3lIwzwzHBOULQovNWyj.jpg",
+    isAnAdult = false,
+    movieGenreDtos = null,
+    overview = "A father and teen daughter attend a pop concert, where they realize they're at the center of a dark and sinister event.",
+    popularity = 790.927,
+    releaseDate = "2024-07-31",
+    video = false,
+    voteAverage = 6.53,
+    voteCount = 968,
+    runtime = null,
+    revenue = null
+)
+
+val expectedMovieDtoList = listOf(expectMovieDto)
+val expectMoviesListResultDto = MoviesListDto(expectedMovieDtoList, 1, 1, 1)
+
+val movieModel = expectMovieDto.toMovie()
+val expectMoviesList = listOf(movieModel)
+val expectedHomeData = HomeData(expectMoviesList, expectMoviesList.map { it.toCarouselDataItem() })
