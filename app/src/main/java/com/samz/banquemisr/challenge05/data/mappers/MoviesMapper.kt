fa2bd68d3e0a87a6f142cc3bf3561efa31cba37e
Bassam Saber <bsaber@panarab-media.com>
2024-09-12 18:26:16 +0300

    - update Movie model - add helper extensions - update movies mapper

--- app/src/main/java/com/samz/banquemisr/challenge05/data/mappers/MoviesMapper.kt
+++ app/src/main/java/com/samz/banquemisr/challenge05/data/mappers/MoviesMapper.kt
@@ -1,10 +1,23 @@
 package com.samz.banquemisr.challenge05.data.mappers
 
+import com.samz.banquemisr.challenge05.core.toDate
+import com.samz.banquemisr.challenge05.core.toFullImageUrl
 import com.samz.banquemisr.challenge05.data.remote.model.MovieDto
 import com.samz.banquemisr.challenge05.domain.model.Movie
 
 fun MovieDto.toMovie(): Movie = Movie(
-    id, title, originalTitle, originalLanguage, posterPath, backdropPath,
-    isAnAdult, movieGenreDtos, overview, popularity, releaseDate,
-    voteAverage, voteCount, runtime
+    id,
+    title,
+    originalTitle,
+    originalLanguage,
+    posterPath?.toFullImageUrl(),
+    backdropPath?.toFullImageUrl(),
+    isAnAdult,
+    movieGenreDtos,
+    overview,
+    popularity ?: 0.0,
+    if (releaseDate.isNullOrEmpty()) null else releaseDate.toDate(),
+    ((voteAverage ?: 0.0) / 10.0).toFloat(),
+    voteCount ?: 0,
+    runtime
 )
